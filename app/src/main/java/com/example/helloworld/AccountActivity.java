510ff76f235fb0caaf31ea52d74d66a142c8c658
Jenný Hildur Ómarsdóttir <jho6@hi.is>
2022-03-04 10:02:03 +0000

    uppsetning

--- app/src/main/java/com/example/helloworld/AccountActivity.java
+++ app/src/main/java/com/example/helloworld/AccountActivity.java
@@ -1,13 +1,13 @@
 package com.example.helloworld;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AccountActivity extends AppCompatActivity {
 
     private Button mheimskButton;
     private Button mklarButton;
@@ -16,18 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mheimskButton = (Button) findViewById(R.id.heimsk_button);
+        mheimskButton = (Button) findViewById(R.id.login);
         mheimskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, R.string.heimsk, Toast.LENGTH_SHORT).show();
+                Toast.makeText(AccountActivity.this, R.string.heimsk, Toast.LENGTH_SHORT).show();
             }
         });
-        mklarButton = (Button) findViewById(R.id.klar_button);
+        mklarButton = (Button) findViewById(R.id.sign_up);
         mklarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, R.string.klar, Toast.LENGTH_SHORT).show();
+                Toast.makeText(AccountActivity.this, R.string.klar, Toast.LENGTH_SHORT).show();
             }
         });
     }
---
+++ app/src/main/java/com/example/helloworld/Quiz.java
@@ -0,0 +1,41 @@
+package com.example.helloworld;
+//import com.google.gson.annotations.SerializedName;
+public class Quiz {
+
+  //  @SerializedName("id")
+    private int mId;
+  //  @SerializedName("questionText")
+    private String mQuestionText;
+  //  @SerializedName("answerTrue")
+    private boolean mAnswerTrue;
+
+    public Quiz(int id, String questionText, boolean answerTrue) {
+        mId = id;
+        mQuestionText = questionText;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public String getQuestionText() {
+        return mQuestionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        mQuestionText = questionText;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
