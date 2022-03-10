a3c44b395852ac8e91ed788d26f33e19db215e9d
Jenný Hildur Ómarsdóttir <jho6@hi.is>
2022-03-10 15:55:48 +0000

    sign up

--- app/src/main/java/com/example/thinkFast/QuizActivity.java
+++ app/src/main/java/com/example/thinkFast/QuizActivity.java
@@ -1,15 +1,20 @@
 package com.example.thinkFast;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class QuizActivity extends AppCompatActivity {
+    private Button mStatistics;
+    private Button mPlayQuiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        mStatistics = (Button) findViewById(R.id.button_statistics);
+        mPlayQuiz = (Button) findViewById(R.id.button_quiz);
     }
 
 }
