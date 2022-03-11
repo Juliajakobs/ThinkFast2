59f746892cdef8550acf668d3d7b017454752336
tommijons <toj6@hi.is>
2022-03-11 10:29:43 +0000

    netw

---
+++ app/src/main/java/com/example/thinkFast/networking/NetworkManager.java
@@ -0,0 +1,38 @@
+package com.example.thinkFast.networking;
+
+import android.content.Context;
+
+import com.android.volley.Network;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class NetworkManager {
+
+    private static final String BASE_URL = "http://10.0.2.2:8080/";
+
+    private static NetworkManager mInstance;
+    private static RequestQueue mQueue;
+    private Context mContext;
+
+    public static synchronized NetworkManager getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new NetworkManager(context);
+        }
+        return mInstance;
+    }
+
+    private NetworkManager(Context context) {
+        mContext = context;
+        mQueue = getRequestQueue();
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mQueue == null) {
+            mQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+        return mQueue;
+    }
+
+
+
+}
