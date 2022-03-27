93347b28c2b3c46f58adff8ef57f42b5f3858dfd
JuliaJakobs <juj7@hi.is>
2022-03-27 16:05:39 +0000

    admin

--- app/src/main/java/com/example/thinkFast/AdminActivity.java
+++ app/src/main/java/com/example/thinkFast/AdminActivity.java
@@ -10,39 +10,50 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.thinkFast.networking.NetworkCallback;
+import com.example.thinkFast.networking.NetworkManager;
+
+import java.util.List;
+
 public class AdminActivity extends AppCompatActivity {
     private Button mDeleteButton;
+    private List<Question> questions;
 
-    private Question[] mQuestions = new Question[]{
-            new Question(0, "Who portrays the character 'Paul' in the film Dune(2021)?", "Timothée Chalamet", "Jason Momoa", "Timothée Chalamet", "Harry Styles", "Dave Bautista"),
-            new Question(0, "Which member of the band 'One Direction' famously left the band on the 25th of March 2015?", "Zayn", "Harry", "Louis", "Neil", "Zayn"),
-            new Question(0, "Who won 'Best Actor' at the Oscars in 2021?", "Anthony Hopkins", "Leonardo DiCaprio", "Anthony Hopkins", "Bob", "Adam Sandler"),
-            new Question(0, "How many seasons are there of the show 'The Bold and the Beautiful'?", "35", "30", "120", "35", "45"),
-            new Question(0, "What is the name of Wario‘s sidekick in the Super Mario franchise?","Waluigi", "Luigi", "Waluigi", "Bowser", "Toad"),
-            new Question(0, "What spell in Harry potter can unlock doors? ", "Alohamora", "Alohamora", "Lumos", "Wingardium Leviosa", "Expelliarmus"),
-            new Question(0, "Which British actor will play Batman in the upcoming reboot?", "Robert Pattinson", "Robert Pattinson", "Ricky Gervais", "Martin Freeman", "James Buckley"),
-            new Question(0, "Which Harry Potter book does not exist? Harry Potter and.. ", "the Sparkling Wands", "the Philosopher's Stone", "the Order of the Phoenix", "the Half-Blood Prince", "the Sparkling Wands"),
-            new Question(0, "Who wrote 'The Lord of the Rings'?", "J.R.R Tolkien", "Dan Brown", "J.K Rowling", "J.R.R Tolkien", "Milan Kundera"),
-            new Question(0, "Who of the following has not been a judge on 'The Great British Bake Off'?", "Jenny Omars", "Jenny Omars", "Prue Leith", "Paul Hollywood", "Mary Berry"),
-    };
-
+    //Changes the view to admin view
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
-        TextView yourTextView = (TextView) findViewById(R.id.textView3); //however your textview is defined in your xml
-        //Displaya dummy data spurningar inn í admin
-        String multiLineText = "";
-        for(int i =0; i< mQuestions.length; i++) {
-            String question = mQuestions[i].getQuestionText();
-            multiLineText = multiLineText + question + "\n" + "\n";
-            yourTextView.setText(multiLineText);
-        }
 
+        TextView yourTextView = (TextView) findViewById(R.id.textView3);
+
+        //Gets questions from database and displays them - not fully complete
+        NetworkManager networkManager = NetworkManager.getInstance(this);
+        networkManager.getQuestions(new NetworkCallback<List<Question>>() {
+            @Override
+            public void onSuccess(List<Question> result) {
+                String multiLineText = "";
+                questions = result;
+                for(int i =0; i<questions.size(); i++ ) {
+                    String question = questions.get(i).getQuestionText();
+                    multiLineText = multiLineText + question + "\n" + "\n";
+                    yourTextView.setText(multiLineText);
+                }
+            }
+
+            @Override
+            public void onFailure(String errorString) {
+                Log.e("lol", "Failed to get questions: " + errorString);
+            }
+        });
+
+
+        //Delete button - not complete
         mDeleteButton = (Button) findViewById(R.id.delete_button);
         mDeleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Should delete selected question from database
                 Toast.makeText(AdminActivity.this, R.string.delete, Toast.LENGTH_SHORT).show();
             }
         });
