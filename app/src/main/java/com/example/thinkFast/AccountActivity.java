20c3afadf764d806cdb83c3e3472d001ae9ad534
Jenný Hildur Ómarsdóttir <jho6@hi.is>
2022-04-12 20:52:27 +0000

    nav merge

--- app/src/main/java/com/example/thinkFast/AccountActivity.java
+++ app/src/main/java/com/example/thinkFast/AccountActivity.java
@@ -5,13 +5,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-import android.view.MenuItem;
-import androidx.annotation.NonNull;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -39,6 +38,7 @@ public class AccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
+        //String uname=sharedpreferences.getString(AccountActivity.uName,"null");
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
 
@@ -53,9 +53,14 @@ public class AccountActivity extends AppCompatActivity {
                 switch(item.getItemId())
                 {
                     case R.id.account:
+                   // TODO user activity
                         return true;
                     case R.id.quiz:
-                        //TODO Only perform if logged in
+                        // Only perform if logged in
+                        if(sharedpreferences.getString(AccountActivity.uName,"null")!="null"){
+                            startActivity(new Intent(AccountActivity.this, SetupActivity.class));
+                        }
+
                         for (int i = 0; i < mAccounts.length; i++) {
                             // Check if user is registered and if admin
                             if (mUsername.getText().toString().equals(mAccounts[i].getUsername()) &&
@@ -65,13 +70,8 @@ public class AccountActivity extends AppCompatActivity {
                                     startActivity(new Intent(AccountActivity.this, AdminActivity.class));
                                 }
                                 else{
-                                    Log.d("MyApp", mUsername.getText().toString() + " " + mAccounts[i].getUsername());
                                     //Sending information about user to QuizActivity
                                     Intent in = new Intent(AccountActivity.this, SetupActivity.class);
-                                    in.putExtra("username", mAccounts[i].getUsername());
-                                    in.putExtra("name", mAccounts[i].getName());
-                                    in.putExtra("email", mAccounts[i].getEmail());
-
                                     startActivity(in);
                                 }}
                         }
