d8d81e50ccfa2186c63d0e1a1a67019254538120
katja8798 <katja8798@gmail.com>
2022-04-21 14:32:52 +0000

    scoreboard virkar

--- app/src/main/java/com/example/thinkFast/ScoreboardActivity.java
+++ app/src/main/java/com/example/thinkFast/ScoreboardActivity.java
@@ -1,7 +1,11 @@
 package com.example.thinkFast;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.thinkFast.networking.NetworkCallback;
@@ -15,10 +19,12 @@ public class ScoreboardActivity extends BaseActivity {
     private TextView mTextView_scores;
     private TextView mTextView_names;
     private NetworkManager networkManager;
-    private List<String> scores;
-    private List <String> usernameList = new ArrayList<>();
-    private List <String> scoreList = new ArrayList<>();
+    private List<String> scores = new ArrayList<String>();
+    private List<String> usernameList = new ArrayList<String>();
+    private List<String> scoreList = new ArrayList<String>();
     private String TAG ="scoreboardActiviity";
+    private LinearLayout nameCol;
+    private LinearLayout scoreCol;
 
     // dummy data
     private Account[] mAccounts = new Account[]{
@@ -49,9 +55,52 @@ public class ScoreboardActivity extends BaseActivity {
                for(int i=0;i<scores.size();i++){
                    usernameList.add(scores.get(i).split(" ")[0]);
                    scoreList.add(scores.get(i).split(" ")[1]);
+                   Log.d(TAG, "Account userList: " + usernameList.get(i));
+                   Log.d(TAG, "Account ScoreList: " + scoreList.get(i));
                 }
                 Log.d(TAG, "Account Username: " + scores.get(0).split(" ")[0]);
                 Log.d(TAG, "Account Score: " + scores.get(0).split(" ")[1]);
+
+                mTextView_scores = (TextView) findViewById(R.id.textView_scores);
+                mTextView_names = (TextView) findViewById(R.id.textView_names);
+                scoreCol = (LinearLayout) findViewById(R.id.scoreColumnScore);
+                nameCol = (LinearLayout) findViewById(R.id.scoreColumnName);
+
+                // TODO: NEED TO DISPLAY IN TEXTVIEW
+                mTextView_names.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
+                mTextView_scores.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
+
+                mTextView_names.setText("user");
+                mTextView_scores.setText("Score");
+
+                LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                );
+
+                for (int i = 0; i < usernameList.size(); i++) {
+                    // Add new textView dynamically to answerColumn 1 and 2
+                    TextView textView1 = new TextView(ScoreboardActivity.this);
+                    TextView textView2 = new TextView(ScoreboardActivity.this);
+
+                    textView1.setLayoutParams(param);
+                    textView2.setLayoutParams(param);
+
+                    textView1.setText(usernameList.get(i));
+                    textView2.setText(scoreList.get(i));
+
+                    textView1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+                    textView2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+
+                    textView2.setTextColor(Color.BLACK);
+
+                    textView1.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                    textView2.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+                    nameCol.addView(textView1);
+                    scoreCol.addView(textView2);
+                }
+                Log.d("myapp","userLength: "+usernameList.size());
             }
 
             @Override
@@ -59,19 +108,5 @@ public class ScoreboardActivity extends BaseActivity {
                 Log.e(TAG, "Failed to get scores: " + errorString);
             }
         });
-
-        mTextView_scores = (TextView) findViewById(R.id.textView_scores);
-        mTextView_names = (TextView) findViewById(R.id.textView_names);
-
-        StringBuilder names = new StringBuilder();
-        StringBuilder scores = new StringBuilder();
-        for (int i = 0; i < usernameList.size(); i++) {
-            names.append(usernameList.get(i)).append("\n");
-            scores.append(scoreList.get(i)).append("\n");
-        }
-        // TODO: NEED TO DISPLAY IN TEXTVIEW
-        mTextView_names.setText("user");
-        mTextView_scores.setText("Score");
     }
-
 }
